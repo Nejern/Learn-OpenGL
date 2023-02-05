885ab3804a740c4465d32d1d624148005cb2b579
Nejern <nejern@yandex.ru>
2023-02-05 21:13:56 +0300

    Added VBO, VAO, vertex shader, fragment shader, shader program and triangle

--- Source.cpp
+++ Source.cpp
@@ -11,7 +11,28 @@
 void key_callback(GLFWwindow *window, int key, int scanCode, int action,
                   int mode);
 
+// Ширина и высота окна
+const GLuint WIDTH = 800, HEIGHT = 600;
+
+// Шейдеры
+// Вершинный шейдер
+const GLchar *vertexShaderSource =
+    "#version 330 core\n"
+    "layout (location = 0) in vec3 position;\n"
+    "void main()\n"
+    "{\n"
+    "gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+    "}\n\0";
+// Фрагментный шейдер
+const GLchar *fragmentShaderSource = "#version 330 core\n"
+                                     "out vec4 color;\n"
+                                     "void main()\n"
+                                     "{\n"
+                                     "color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                     "}\n\0";
+
 int main(void) {
+  /* GLFW */
   // Инициализация GLFW
   glfwInit();
   // Настройка GLFW
@@ -28,19 +49,20 @@ int main(void) {
 
   // Создание окна
   GLFWwindow *window =
-      glfwCreateWindow(800, 600, "Learn OpenGL", nullptr, nullptr);
+      glfwCreateWindow(WIDTH, HEIGHT, "Learn OpenGL", nullptr, nullptr);
   if (window == nullptr) {
     std::cout << "Failed to create GLFW window" << std::endl;
     glfwTerminate();
     return -1;
   }
+  // Установка текущего контекста OpenGL
+  glfwMakeContextCurrent(window);
 
-  // Регистрация колбэков
+  /* Колбэки */
   // Нажатие клавиш клавиатуры
   glfwSetKeyCallback(window, key_callback);
 
-  // Установка текущего контекста OpenGL
-  glfwMakeContextCurrent(window);
+  /* GLEW */
   // Инициализация GLEW
   glewExperimental = GL_TRUE;
   if (glewInit() != GLEW_OK) {
@@ -49,18 +71,96 @@ int main(void) {
   }
 
   // Определение размеров окна
-  int width, height;
+  GLint width, height;
   glfwGetFramebufferSize(window, &width, &height);
   glViewport(0, 0, width, height);
 
+  /* Шейдеры */
+  /* Вершинный шейдер */
+  // Создание шейдера
+  GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  // Передача исходного кода шейдера
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  // Компиляция шейдера
+  glCompileShader(vertexShader);
+  // Проверка на ошибки компиляции
+  GLint success;
+  GLchar infoLog[512];
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+              << infoLog << std::endl;
+  }
+
+  /* Фрагментный шейдер */
+  // Создание шейдера
+  GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  // Передача исходного кода шейдера
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+  // Компиляция шейдера
+  glCompileShader(fragmentShader);
+  // Проверка на ошибки компиляции
+  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
+              << infoLog << std::endl;
+  }
+
+  /* Шейдерная программа */
+  // Создание шейдерной программы
+  GLuint shaderProgram = glCreateProgram();
+  // Прикрепление шейдеров к шейдерной программе
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  // Связывание шейдеров в шейдерную программу
+  glLinkProgram(shaderProgram);
+  // Проверка на ошибки связывания
+  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+  if (!success) {
+    glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n"
+              << infoLog << std::endl;
+  }
+  // Удаление шейдеров
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+
+  /* Вершинный буфер (VBO) */
+  // Вершины треугольника
+  GLfloat vertices[] = {// Позиции
+                        -0.5f, -0.5f, 0.0f, 0.5f, -0.5f,
+                        0.0f,  0.0f,  0.5f, 0.0f};
+
+  /* VBO & VAO */
+  // Создание VBO
+  GLuint VBO;
+  glGenBuffers(1, &VBO);
+  // Создание VAO
+  GLuint VAO;
+  glGenVertexArrays(1, &VAO);
+  // Привязка VAO
+  glBindVertexArray(VAO);
+  // Привязка VBO
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  // Копирование массива вершин в буфер
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  // Установка указателей на вершинные атрибуты
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat),
+                        (GLvoid *)0);
+  glEnableVertexAttribArray(0);
+  // Отвязка VAO
+  glBindVertexArray(0);
+
   // Цвет очистки экрана
-  float red, green, blue, alpha;
+  GLfloat red, green, blue, alpha;
   red = 49.f / 255.f;
   green = 0.f;
   blue = 98.f / 255.f;
   alpha = 1.f;
 
-  // Цикл отрисовки
+  /* Цикл отрисовки */
   while (!glfwWindowShouldClose(window)) {
     // Проверка наличия событий
     glfwPollEvents();
@@ -68,6 +168,15 @@ int main(void) {
     // Очистка буфера цвета
     glClearColor(red, green, blue, alpha);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    // Использование шейдерной программы
+    glUseProgram(shaderProgram);
+    // Привязка VAO
+    glBindVertexArray(VAO);
+    // Отрисовка треугольника
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    // Отвязка VAO
+    glBindVertexArray(0);
 
     // Замена цветового буфера
     glfwSwapBuffers(window);
