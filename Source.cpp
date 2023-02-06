80e1101586c60d180498ee59351949d9dbb52458
Nejern <nejern@yandex.ru>
2023-02-06 17:28:12 +0300

    Changed the clean up color, the shape has been changed to a triangle

--- Source.cpp
+++ Source.cpp
@@ -17,20 +17,22 @@ const GLuint WIDTH = 800, HEIGHT = 600;
 
 // Шейдеры
 // Вершинный шейдер
-const GLchar *vertexShaderSource =
-    "#version 460 core\n"
-    "layout (location = 0) in vec3 position;\n"
-    "void main()\n"
-    "{\n"
-    "gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
-    "}\n\0";
+const GLchar *vertexShaderSource = "#version 460 core\n"
+                                   "layout (location = 0) in vec3 position;\n"
+                                   "layout (location = 1) in vec3 color;\n"
+                                   "out vec3 ourColor;\n"
+                                   "void main()\n"
+                                   "{\n"
+                                   "gl_Position = vec4(position, 1.0);\n"
+                                   "ourColor = color;\n"
+                                   "}\n\0";
 // Фрагментный шейдер
 const GLchar *fragmentShaderSource = "#version 460 core\n"
+                                     "in vec3 ourColor;\n"
                                      "out vec4 color;\n"
-                                     "uniform vec4 ourColor;\n"
                                      "void main()\n"
                                      "{\n"
-                                     "color = ourColor;\n"
+                                     "color = vec4(ourColor, 1.0f);\n"
                                      "}\n\0";
 
 int main(void) {
@@ -132,19 +134,13 @@ int main(void) {
   // Вершины прямоугольника
   // Позиции вершин
   GLfloat vertices[] = {
-      // Прямоугольник
-      0.5f,  0.5f,  0.0f, // Верхний правый угол
-      0.5f,  -0.5f, 0.0f, // Нижний правый угол
-      -0.5f, -0.5f, 0.0f, // Нижний левый угол
-      -0.5f, 0.5f,  0.0f  // Верхний левый угол
-  };
-  // Индексы вершин
-  GLuint indices[] = {
-      0, 1, 3, // Первый треугольник
-      1, 2, 3  // Второй треугольник
+      // Прямоугольник    // Цвет
+      0.0f,  0.5f,  0.0f, 1.0f, 0.0f, 0.0f, // Верхний угол
+      0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // Нижний правый угол
+      -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // Нижний левый угол
   };
 
-  /* VBO & VAO & EBO */
+  /* VBO & VAO */
 
   // Создание VBO
   GLuint VBO;
@@ -152,42 +148,31 @@ int main(void) {
   // Создание VAO
   GLuint VAO;
   glGenVertexArrays(1, &VAO);
-  // Создание EBO
-  GLuint EBO;
-  glGenBuffers(1, &EBO);
 
   // Привязка VAO
   glBindVertexArray(VAO);
   // Привязка VBO
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   // Копирование массива вершин в буфер
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-  // Установка указателей на вершинные атрибуты
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat),
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
+  // Указание атрибутов вершин
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat),
                         (GLvoid *)0);
+  // Указание атрибутов цвета
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat),
+                        (GLvoid *)(3 * sizeof(GLfloat)));
   // Включение вершинных атрибутов
   glEnableVertexAttribArray(0);
-
-  // Привязка EBO
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-  // Копирование массива индексов в буфер
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
-               GL_STATIC_DRAW);
+  // Включение атрибутов цвета
+  glEnableVertexAttribArray(1);
 
   // Отвязка VBO
   glBindBuffer(GL_ARRAY_BUFFER, 0);
   // Отвязка VAO
   glBindVertexArray(0);
-  // Отвязка EBO
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
   // Цвет очистки экрана
-  GLfloat red, green, blue, alpha;
-  red = 49.f / 255.f;
-  green = 0.f;
-  blue = 98.f / 255.f;
-  alpha = 1.f;
-  glClearColor(red, green, blue, alpha);
+  glClearColor(29.f/255, 32.f/255, 33.f/255, 1.f);
 
   /* Цикл отрисовки */
   while (!glfwWindowShouldClose(window)) {
@@ -201,14 +186,10 @@ int main(void) {
     // Использование шейдерной программы
     glUseProgram(shaderProgram);
     // Прямоугольник
-    // Изменение цвета в зависимости от времени
-    GLfloat greenValue = (cos(glfwGetTime()) / 2) + 0.5;
-    GLint vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-    glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
     // Привязка VAO
     glBindVertexArray(VAO);
     // Отрисовка примитивов
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
     // Отвязка VAO
     glBindVertexArray(0);
 
@@ -227,8 +208,6 @@ int main(void) {
   glDeleteVertexArrays(1, &VAO);
   // Удаление VBO
   glDeleteBuffers(1, &VBO);
-  // Удаление EBO
-  glDeleteBuffers(1, &EBO);
   // Удаление шейдерной программы
   glDeleteProgram(shaderProgram);
   // Освобождение ресурсов
